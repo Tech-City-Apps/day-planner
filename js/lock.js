98b8f76270295ad42c5055791762c284a3412996
MICHAEL ROARK <Roarkmichael4@gmail.com>
2024-03-11 11:24:05 -0700

    changed trasition time

--- js/lock.js
+++ js/lock.js
@@ -1,12 +1,12 @@
 
 function flipNotebook() {
-  const notebook = document.querySelector('.notebook-inner');
+  let notebook = document.querySelector('.notebook-inner');
   notebook.style.transform = 'rotateY(180deg)'; // Flip the notebook
 
   // Wait for 1 second to show the back cover, then redirect
   setTimeout(() => {
     window.location.href = 'index.html'; // Adjust the URL as needed
-  }, 300); // Adjust delay as needed
+  }, 350); // Adjust delay as needed
 }
 
 
