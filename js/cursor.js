3f03084df02fcf8bc547adc8267ac9955417d0c6
Vivian Davila <viviananddav@gmail.com>
2024-03-12 11:02:24 -0700

    Edits to add notebook background

--- js/cursor.js
+++ js/cursor.js
@@ -10,20 +10,10 @@ if (document.getElementById('notebook')) {
   // Remove class to revert to default cursor styles
   document.body.classList.remove('on-open-page');
 
-  // Function to change cursor to a pen on list item hover
-  function changeCursorToPen() {
-    const listItems = document.getElementById('taskList').getElementsByTagName('li');
-
-    for (const listItem of listItems) {
-      listItem.classList.add('custom-cursor-pen');
-
-      listItem.addEventListener('mouseout', function () {
-        listItem.classList.remove('custom-cursor-pen');
-      });
-    }
+  // Add custom cursor class to list items
+  const listItems = document.getElementById('taskList').getElementsByTagName('li');
+  for (const listItem of listItems) {
+    listItem.classList.add('custom-cursor-pen');
   }
-
-  // Call the function to apply the pen cursor to list items
-  changeCursorToPen();
 }
 
